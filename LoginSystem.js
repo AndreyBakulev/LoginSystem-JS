914014f8e4f555041292429d2e959bc85952da16
AndreyBakulev <gengubae@gmail.com>
2024-03-06 13:10:02 -0500

    trying to do ban timer

--- LoginSystem.js
+++ LoginSystem.js
@@ -139,39 +139,49 @@ app.get("/", requireAuth, async (req, res) => {
 // Login route
 app.post("/login", async (req, res) => {
   const { newUsername, newPassword } = req.body;
-  if (loginAttmepts < 3) {
-    rememberMe = req.body.rememberMe === "true"; // will be 'true' if checked, undefined if not
-
-    // Check if the provided username and password are valid
-    const uniqueSalt = await checkIfExists(newUsername, "username", "salt");
-    //if the username and pass exist in the userbase
-    if (
-      (await checkIfExists(newUsername, "username")) &&
-      (await checkIfExists(hash(newPassword + uniqueSalt), "password"))
-    ) {
+  console.log(await checkIfExists(newUsername, "username", "timeout"));
+  if (
+    await checkIfExists(newUsername, "username", "timeout") < d.getTime() ||
+    await checkIfExists(newUsername, "username", "timeout") === 0.0
+  ) {
+    //set timer back to 0
+    setInDatabase(newUsername, "timeout", 0);
+    if (loginAttmepts < 3) {
+      rememberMe = req.body.rememberMe === "true"; // will be 'true' if checked, undefined if not
+      // Check if the provided username and password are valid
+      const uniqueSalt = await checkIfExists(newUsername, "username", "salt");
+      //if the username and pass exist in the userbase
       if (
-        (await checkIfExists(newUsername, "username", "banned")) === "false"
+        (await checkIfExists(newUsername, "username")) &&
+        (await checkIfExists(hash(newPassword + uniqueSalt), "password"))
       ) {
-        //check if they are banned
-        //saying they have successfully logged in
-        const userId = await checkIfExists(newUsername, "username", "id");
-        //this just checks if the user wants to stay logged in
-        req.session.userId = userId;
-        res.redirect("/");
+        if (
+          (await checkIfExists(newUsername, "username", "banned")) === "false"
+        ) {
+          //check if they are banned
+          //saying they have successfully logged in
+          const userId = await checkIfExists(newUsername, "username", "id");
+          //this just checks if the user wants to stay logged in
+          req.session.userId = userId;
+          res.redirect("/");
+        } else {
+          console.log("This user is banned!");
+        }
       } else {
-        console.log("This user is banned!");
+        // Redirect to the login page with a flag indicating login failure
+        console.log("Invalid username or password!");
+        res.render("login");
+        loginAttmepts++;
       }
     } else {
-      // Redirect to the login page with a flag indicating login failure
-      console.log("Invalid username or password!");
-      res.render("login");
-      loginAttmepts++;
+      const idOfUsername = await checkIfExists(newUsername, "username", "id");
+      setInDatabase(idOfUsername, "timeout", d.getTime() + 1000 * 60 * 60);
+      res.send("Too many login attempts. Please try again in an hour.");
     }
   } else {
-    const idOfUsername = await checkIfExists(newUsername, "username", "id");
-    setInDatabase(idOfUsername, "timeout", d.getTime() + 1000 * 60 * 60);
-    res.send("Too many login attempts. Please try again in an hour.");
-    //sets the unbanned time
+    const timeLeft = (await checkIfExists(newUsername, "username", "timeout") - d.getTime())/60000;
+    console.log("you have been timed out for too many login attempts!");
+    console.log("you have "+timeLeft +" minutes left");
   }
 });
 
